63d1ea3aeb421eedd64eec872d7dc15a82d7e69e
pushpakbv <149048626+pushpakbv@users.noreply.github.com>
2024-09-22 16:52:17 +0530

    Add files via upload

---
+++ src/pages/AddPost.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Container,PostForm } from '../components'
+
+function AddPost() {
+  return (
+    <div className='py=8'>
+        <PostForm />
+    </div>
+  )
+}
+
+export default AddPost
